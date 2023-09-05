f58f6d45d04c48dbe95c8a3e38559e6066c58dbd
Sheikh Irfan <142287554+SheikhIrfan89@users.noreply.github.com>
2023-09-05 08:22:37 +0530

    Added DB context Entity framework.

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Types> Types { get; set; }
+
+
+    }
+}
